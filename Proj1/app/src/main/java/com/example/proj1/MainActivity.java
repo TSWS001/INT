6cbb75e0ef9c07720f8d1fc60c126a3cdfeaecb5
Xinlei <xinlei0116@gmail.com>
2022-10-10 13:05:26 +0200

    xinlei 10/10/22 13:05

---
+++ Proj1/app/src/main/java/com/example/proj1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.proj1;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+        TextView title = findViewById(R.id.toolbar_title);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startactivity()
+                Toast.makeText(MainActivity, "", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
