51ed9adee4900bb6bd1fa8cb534993ffb2d0b180
Xinlei <xinlei0116@gmail.com>
2022-10-20 10:14:19 +0200

    xinlei 10/10/22 10:14

--- Proj1/app/src/main/java/com/example/proj1/cards.java
+++ Proj1/app/src/main/java/com/example/proj1/cards.java
@@ -23,7 +23,7 @@ public class cards extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.toolbar);
+        setContentView(R.layout.productlist);
 
         RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
         //        SetUpProducts();
