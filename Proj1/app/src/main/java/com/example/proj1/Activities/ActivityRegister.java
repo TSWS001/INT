bca3a2019e20d9ba6da65bfb5764d63f5c18a0cb
Xinlei <xinlei0116@gmail.com>
2022-11-17 10:00:57 +0100

    xinlei 17/11/22 10.00

--- Proj1/app/src/main/java/com/example/proj1/Activities/ActivityRegister.java
+++ Proj1/app/src/main/java/com/example/proj1/Activities/ActivityRegister.java
@@ -1,6 +1,9 @@
 package com.example.proj1.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,7 +14,7 @@ import com.example.proj1.R;
 
 public class ActivityRegister extends AppCompatActivity {
 
-    Button btnAceptar;
+    Button btnAceptar,btnlogin;
     EditText first_name,last_name, email, password, birth_day, birth_month, birth_year,prefix_phone, phone, address;
     //habria que hacer un atributo de la clase customer de si el usuario esta o no registrado
     @Override
@@ -19,6 +22,7 @@ public class ActivityRegister extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_register);
 
+        btnlogin = findViewById(R.id.btnlogin);
         btnAceptar = findViewById(R.id.btnaceptar_register);
         first_name = findViewById(R.id.textfirstname);
         last_name = findViewById(R.id.textlastname);
@@ -30,6 +34,21 @@ public class ActivityRegister extends AppCompatActivity {
         prefix_phone = findViewById(R.id.text_prefix_phone);
         phone = findViewById(R.id.text_phone);
         address = findViewById(R.id.text_direccion);
+
+        btnAceptar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                SetUserData();
+                startActivity(new Intent(ActivityRegister.this,MainActivity.class));
+            }
+        });
+
+        btnlogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityRegister.this,ActivityLogin.class));
+            }
+        });
 
     }
 
@@ -44,8 +63,17 @@ public class ActivityRegister extends AppCompatActivity {
         String address_data = address.getText().toString();
 
         Customer user = new Customer(first_name_data, last_name_data, email_data, password_data);
-        user.setBirth_date(birth_data);
+        user.setBirth_date(birth_data);//comprobar la fecha
         user.setPhone(phone_data);
         user.setAddress(address_data);
+
+        Log.i("first_name:",first_name_data);
+        Log.i("last_name_data:",last_name_data);
+        Log.i("email_data:",email_data);
+        Log.i("password_data:",password_data);
+        Log.i("birth_data:",birth_data);
+        Log.i("phone_data:",phone_data);
+        Log.i("address_data:",address_data);
+
     }
 }
